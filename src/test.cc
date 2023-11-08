03c9b0f14ba2e4135c1a6808f43f343398861044
confucuis <confucuis@github.com>
2023-11-08 15:40:29 +0800

    ..

--- src/test.cc
+++ src/test.cc
@@ -1,14 +1,19 @@
 #include "test.h"
+#include <iostream>
 
+using namespace std;
+
+// 构造函数
 Test::Test(int a, int b)
 {
     this->a = a;
     this->b = b;
 }
 
+// 析构函数
 Test::~Test(void)
 {
-
+    std::cout << "析构函数: 无参数,无返回值\n";
 }
 
 int Test::add(void)
