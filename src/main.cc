a34f962c0b905ef439167c5d3f1d58e1c9ae6363
confucuis <confucuis@github.com>
2023-11-09 10:51:26 +0800

    ..

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "calculate.h"
 
+// 混编译在c++文件中
+// 用以下方式引入c头文件
+extern "C" {
+    #include "person.h"
+}
+
 using namespace std;
 
 int main(void)
@@ -10,6 +16,10 @@ int main(void)
     std::cout << c.sub() << endl;
     std::cout << c.mul() << endl;
     std::cout << c.div() << endl;
+    
+    const Person person = {.num = 12, .name = "张三"};
+    say_num(person);
+    say_name(person);
 
     return 0;
 }
